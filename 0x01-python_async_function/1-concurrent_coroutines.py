37c24a25becef516b3ff465cb40f3a95d007751a
Excel Nwachukwu <trillionclues@gmail.com>
2023-01-10 00:07:52 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 from typing import 
 
-wait_random = __import__('0-basic_async_syntax').
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
